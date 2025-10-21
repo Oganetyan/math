c49939eec198e4521d2da2682570abc8b7c83c3d
oganetyan <timofeev.nikita12@yandex.ru>
2025-10-21 10:22:49 +1000

    Spelling in tests has been corrected

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,20 +4,20 @@
 
 int main()
 {
-    std::cout << "=== Triganometric test start ===" << std::endl
+    std::cout << "=== Trigonometric test start ===" << std::endl
               << "Sin test: " << math::sin(math::PI / 4.0) << " " << std::sin(M_PI / 4.0) << std::endl
               << "Cos test: " << math::cos(math::PI / 4.0) << " " << std::cos(M_PI / 4.0) << std::endl
               << "Tan test: " << math::tan(math::PI / 4.0) << " " << std::tan(M_PI / 4.0) << std::endl
-              << "=== Triganometric test compited ===" << std::endl;
+              << "=== Trigonometric test complyted ===" << std::endl;
 
     std::cout << std::endl;
-    
-    std::cout << "=== Powers functions test start ===" << std::endl
-              << "Exponenta test: " <<math::exp(1.5) << " " << std::exp(1.5) << std::endl
-              << "Ln test: " <<math::ln(math::E) << " " << std::log(M_E) << std::endl
-              << "Pow test: " <<math::pow(math::PI, math::E) << " " << std::pow(M_PI, M_E) << std::endl
-              << "Log test: " <<math::log(2,1024) << " " << std::log2(1024) << std::endl
-              << "=== Powers functions test complited === " <<std::endl;
 
-        return 0;
+    std::cout << "=== Powers functions test start ===" << std::endl
+              << "Exponent test: " << math::exp(1.5) << " " << std::exp(1.5) << std::endl
+              << "Ln test: " << math::ln(math::E) << " " << std::log(M_E) << std::endl
+              << "Pow test: " << math::pow(math::PI, math::E) << " " << std::pow(M_PI, M_E) << std::endl
+              << "Log test: " << math::log(2, 1024) << " " << std::log2(1024) << std::endl
+              << "=== Powers functions test complyted === " << std::endl;
+
+    return 0;
 }
